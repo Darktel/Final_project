57ab62808a5915af805ef3bfeea79abdd6337ac7
gusev_ae <gusev_ae@magnit.ru>
2025-12-27 23:19:19 +0300

    Почистил файл ClшentAnnouncement от не используемых кусков кода.

--- src/test/java/ru/praktikumservices/stand/qadesk/client/ClientAnnouncement.java
+++ src/test/java/ru/praktikumservices/stand/qadesk/client/ClientAnnouncement.java
@@ -4,9 +4,7 @@ import io.restassured.response.Response;
 import ru.praktikumservices.stand.qadesk.base.BaseHttpClient;
 import ru.praktikumservices.stand.qadesk.models.Announcement;
 
-import java.util.Map;
 
-import static io.restassured.RestAssured.authentication;
 import static io.restassured.RestAssured.given;
 
 public class ClientAnnouncement {
@@ -19,10 +17,6 @@ public class ClientAnnouncement {
         String city = announcement.getCity();
         String description = announcement.getDescription();
         String price = announcement.getPrice();
-//        byte[] image1 = announcement.getImage1();
-//        byte[] image2 = announcement.getImage2();
-//        byte[] image3 = announcement.getImage3();
-
 
         Response response = given()
                 .header("Accept", "application/json, text/plain, */*")
@@ -37,50 +31,8 @@ public class ClientAnnouncement {
                 .multiPart("city", city)
                 .multiPart("description", description)
                 .multiPart("price", price)
-//                .body(Map.of(
-//                        "name", name,
-//                        "category", category,
-//                        "condition", condition,
-//                        "city", city,
-//                        "description", description,
-//                        "price", price
-//                ))
                 .post();
-
         // Проверяем, что запрос успешен (201 Created или 200 OK)
         response.then().statusCode(201);
     }
-//
-//    RestAssured.baseURI = "https://qa-desk.stand.praktikum-services.ru";
-//
-//    File image = new File("src/test/resources/donut.png"); // Убедись, что файл есть
-//
-//    given()
-//            .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQ4MDgsImVtYWlsIjoia2VuLnJvd2VAeWFob28uY29tIiwibmFtZSI6IlVzZXIiLCJpYXQiOjE3NjY1MjUxMjUsImV4cCI6MTc2NjYxMTUyNX0.boDwh2Urbv9GCGUhojUgscaIiZckUdofuz9BVlcd70w")
-//            .cookie("_yasc", "3WdPSSvj3SG/2cuOCBzpvAqvqzE2ZxKMrXA0ZMsXEN7p4TZ/nBBxox10acr5uGX7")
-//            .header("Accept", "application/json, text/plain, */*")
-//            .header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
-//            .header("Origin", "https://qa-desk.stand.praktikum-services.ru")
-//            .header("Referer", "https://qa-desk.stand.praktikum-services.ru/create-lisiting")
-//            .header("DNT", "1")
-//            .header("sec-ch-ua", "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"")
-//            .header("sec-ch-ua-mobile", "?0")
-//            .header("sec-ch-ua-platform", "\"macOS\"")
-//            .multiPart("name", "название")
-//            .multiPart("category", "Авто")
-//            .multiPart("condition", "Б/У")
-//            .multiPart("city", "Москва")
-//            .multiPart("description", "123")
-//            .multiPart("price", 444)
-//            .multiPart("images", image, "image/png")
-//            .multiPart("images", image, "image/png")
-//            .multiPart("images", image, "image/png")
-//            .log().all()
-//        .when()
-//            .post("/api/create-listing")
-//        .then()
-//            .log().body()
-//            .statusCode(200); // или другой ожидаемый статус
-//}
-
 }
